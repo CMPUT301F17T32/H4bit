5c4db06c6613c75da422c05d60cdc628e802f953
bhlewka <ben.hlewka@hotmail.com>
2017-10-29 20:59:17 -0600

    create account activity skeletoned a bit?

---
+++ H4bit/app/src/main/java/h4bit/h4bit/CreateAccountActivity.java
@@ -0,0 +1,13 @@
+package h4bit.h4bit;
+
+/**
+ * Created by benhl on 2017-10-29.
+ */
+
+/**
+ * usernameText, passwordText, signupButton, crealogButton
+ * These are the respective textboxes required to handle the information
+ */
+
+public class CreateAccountActivity {
+}
