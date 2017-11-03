a750bf4bca1db1f66fb9d6f427c3f6f127e12e51
Benjamin Hlewka <bhlewka@cs.ualberta.ca>
2017-11-03 13:51:48 -0600

    Main activity stuff

--- H4bit/app/src/main/java/h4bit/h4bit/MainHabitActivity.java
+++ H4bit/app/src/main/java/h4bit/h4bit/MainHabitActivity.java
@@ -2,6 +2,8 @@ package h4bit.h4bit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -15,5 +17,18 @@ public class MainHabitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_habit);
+
+        Button historyButton = (Button) findViewById(R.id.historyButton);
+        Button socialButton = (Button) findViewById(R.id.socialButton);
+
+        // The habitsButton should do nothing on this screen
+        // (Because it takes us to where we already are)
+
+        // This is the listener for the historyButton press
+        //
+        newCounter.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+
+
     }
 }
