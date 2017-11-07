e9618edc39032ffc4c170367827a8a2b37e3abaa
Popine <apriest@ualberta.ca>
2017-11-06 19:24:00 -0700

    Update HabitEventList.java

--- H4bit/app/src/main/java/h4bit/h4bit/HabitEventList.java
+++ H4bit/app/src/main/java/h4bit/h4bit/HabitEventList.java
@@ -3,13 +3,22 @@ package h4bit.h4bit;
 import java.util.ArrayList;
 
 /**
- * Created by bhlewka on 11/3/17.
+ * Created by Alex on 2017-11-03.
  */
 
-public class HabitEventList extends HabitList{
-    private ArrayList<HabitEvent> habitEventArrayList;
+public class HabitEventList {
+    private ArrayList<HabitEvent> habitEventList;
+
+    public HabitEventList(){
+        habitEventList = new ArrayList<HabitEvent>();
+    }
 
     public void addHabitEvent(HabitEvent habitEvent){
-        this.habitEventArrayList.add(habitEvent);
+        this.habitEventList.add(habitEvent);
     }
+
+    public void deleteHabitEvent(HabitEvent habitEvent){
+        this.habitEventList.remove(habitEvent);
+    }
+
 }
