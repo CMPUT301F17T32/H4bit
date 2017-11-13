24dbff4f72c9609a87a3fd3868bce37d6fab4f95
bhlewka <ben.hlewka@hotmail.com>
2017-11-13 01:06:48 -0700

    made more test pages

---
+++ H4bit/app/src/test/java/h4bit/h4bit/FollowTest.java
@@ -0,0 +1,9 @@
+package h4bit.h4bit;
+
+/**
+ * Created by benhl on 2017-11-13.
+ */
+
+public class FollowTest {
+
+}
--- H4bit/app/src/test/java/h4bit/h4bit/UserTest.java
+++ H4bit/app/src/test/java/h4bit/h4bit/UserTest.java
@@ -27,5 +27,4 @@ public class UserTest{
 //        }
         assertTrue(request);
     }
-
 }
