91955f07d6072f999ee66684213633a58de8f775
bhlewka <ben.hlewka@hotmail.com>
2017-12-03 15:58:08 -0700

    elastic search ???

--- H4bit/app/src/main/java/h4bit/h4bit/Views/CreateHabitActivity.java
+++ H4bit/app/src/main/java/h4bit/h4bit/Views/CreateHabitActivity.java
@@ -30,6 +30,7 @@ import java.util.Date;
 import java.util.Objects;
 
 import h4bit.h4bit.Controllers.SaveLoadController;
+import h4bit.h4bit.Models.ElasticSearch;
 import h4bit.h4bit.Models.Habit;
 import h4bit.h4bit.Controllers.HabitController;
 import h4bit.h4bit.Models.HabitList;
@@ -52,6 +53,7 @@ public class CreateHabitActivity extends AppCompatActivity {
     private String mode;
     private int position;
     private SaveLoadController saveLoadController;
+    private ElasticSearch elasticSearch = new ElasticSearch();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +70,20 @@ public class CreateHabitActivity extends AppCompatActivity {
         this.saveLoadController = new SaveLoadController(savefile, this.getApplicationContext());
 
         // load the user from the savefile
-        user = saveLoadController.load();
+        // Make sure you use the more recent user object
+        User user1 = saveLoadController.load();
+        try {
+            User user2 = elasticSearch.getUser(savefile.substring(0, savefile.length() - 4));
+            // Compare recent updated
+            if (user1.getLastModified().getTime() > user2.getLastModified().getTime()){
+                user = user1;
+            } else {
+                user = user2;
+            }
+        } catch (Exception e) {
+            user = user1;
+        }
+
 
         // init the habit controller
         this.habitController = new HabitController();
@@ -239,6 +254,7 @@ public class CreateHabitActivity extends AppCompatActivity {
         }
         // do nothing if edit returns -1
 
+        elasticSearch.updateUser(user);
         saveLoadController.save(user);
         finish();
     }
